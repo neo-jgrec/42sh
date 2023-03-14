837567e4795c6819f14b6efc4190e5c077158ab1
JGrec <jean-yanis.jeffroy@epitech.eu>
2023-03-14 14:46:51 +0100

    fix(main) cd norm and function mdoe

--- src/builtins/my_cd.c
+++ src/builtins/my_cd.c
@@ -8,6 +8,19 @@
 #include "my.h"
 #include <string.h>
 
+static void error_handling(term_t *term)
+{
+    if (errno != 0) {
+        my_printf("%s: %s.\n", term->argv[1], strerror(errno));
+        errno = 0;
+    } else {
+        term->old_cwd = term->cwd;
+        term->cwd = getcwd(NULL, 0);
+        my_setenv("OLDPWD", term->old_cwd, term->env);
+        my_setenv("PWD", term->cwd, term->env);
+    }
+}
+
 void my_cd(term_t *term)
 {
     char *tmp = NULL;
@@ -20,16 +33,13 @@ void my_cd(term_t *term)
         tmp = my_getenv(term->env, "HOME");
         if (tmp != NULL)
             chdir(tmp);
-        else
+        else {
             my_printf("cd: No home directory.\n");
+            return;
+        }
     } else if (my_strcmp(term->argv[1], "-") == 0)
         chdir(term->old_cwd);
     else
         chdir(term->argv[1]);
-    if (errno != 0) {
-        my_printf("%s: %s.\n", term->argv[1], strerror(errno));
-        errno = 0;
-    }
-    term->old_cwd = term->cwd;
-    term->cwd = getcwd(NULL, 0);
+    error_handling(term);
 }

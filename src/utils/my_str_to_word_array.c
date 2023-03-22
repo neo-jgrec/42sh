ae91c1d788ccd617bd1f1688289ce29e5a53ca0e
JGrec <jean-yanis.jeffroy@epitech.eu>
2023-03-22 17:53:46 +0100

    feat(main) quotes handling

--- src/utils/my_str_to_word_array.c
+++ src/utils/my_str_to_word_array.c
@@ -7,27 +7,48 @@
 
 #include "my.h"
 
-static char *fill_line(char const *str, int pos, char const separator)
+#include <stdlib.h>
+#include <stdbool.h>
+
+static char *fill_line(char const *str, int *pos, char const separator)
 {
-    int tmp = pos;
     int len = 0;
     char *line;
+    bool in_quotes = false;
+    int tmp = *pos;
 
-    for (; str[pos] != separator && str[pos] != '\0'; pos++)
+    for (; (str[tmp] != separator && str[tmp] != '\0') || in_quotes; tmp++) {
+        if (str[tmp] == '"') {
+            in_quotes = !in_quotes;
+            continue;
+        }
         len++;
-    line = malloc(sizeof(char) * (len + 3));
-    for (int pos = 0; str[tmp] != separator && str[tmp] != '\0'; pos++, tmp++)
-        line[pos] = str[tmp];
+    }
+    line = malloc(sizeof(char) * (len + 1));
+
+    for (int i = 0; (str[*pos] != separator && str[*pos] != '\0') || in_quotes; (*pos)++) {
+        if (str[*pos] == '"') {
+            in_quotes = !in_quotes;
+            continue;
+        }
+        line[i++] = str[*pos];
+    }
     line[len] = '\0';
     return line;
 }
 
-static int count_word(char const *str, int pos, char const separator)
+static int count_word(char const *str, int *pos, char const separator)
 {
     int letter = 0;
+    bool in_quotes = false;
 
-    for (;str[pos] != separator && str[pos] != '\0'; pos++)
+    for (; (str[*pos] != separator && str[*pos] != '\0') || in_quotes; (*pos)++) {
+        if (str[*pos] == '"') {
+            in_quotes = !in_quotes;
+            continue;
+        }
         letter++;
+    }
     if (letter != 0)
         return 1;
     return 0;
@@ -37,14 +58,14 @@ static char **fill_tab(char const *str, char const separator, int nbr_word)
 {
     int pos_tab = 0;
     char **tab = malloc(sizeof(char *) * (nbr_word + 1));
+    int pos = 0;
 
     if (!tab)
         return NULL;
-    for (int pos = 0; str[pos] != '\0' && nbr_word != 0;) {
+    while (str[pos] != '\0' && nbr_word != 0) {
         for (;str[pos] == separator && str[pos] != '\0'; pos++);
-        tab[pos_tab] = fill_line(str, pos, separator);
+        tab[pos_tab] = fill_line(str, &pos, separator);
         pos_tab++;
-        for (;str[pos] != separator && str[pos] != '\0'; pos++);
     }
     tab[nbr_word] = NULL;
     return tab;
@@ -53,11 +74,12 @@ static char **fill_tab(char const *str, char const separator, int nbr_word)
 char **my_str_to_word_array(char *str, char c)
 {
     int nbr_word = 0;
+    int pos = 0;
 
     if (str == NULL)
         return NULL;
-    for (int pos = 0; str[pos] != '\0';) {
-        nbr_word += count_word(str, pos, c);
+    while (str[pos] != '\0') {
+        nbr_word += count_word(str, &pos, c);
         for (;str[pos] != c && str[pos] != '\0'; pos++);
         for (;str[pos] == c && str[pos] != '\0'; pos++);
     }

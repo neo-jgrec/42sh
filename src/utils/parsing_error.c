08d4723460b89f5834630e6e6382c72b2efcd655
Jean-Yanis Jeffroy <jean-yanis.jeffroy@epitech.eu>
2023-03-23 12:25:31 +0100

    fix(main) 0

--- src/utils/parsing_error.c
+++ src/utils/parsing_error.c
@@ -10,21 +10,16 @@
 
 int parsing_error(char **args)
 {
-    int i = 0;
-    int j = 0;
-
-    while (args[i] != NULL) {
-        if ((my_strcmp(args[i], ">") == 0 || my_strcmp(args[i], "<") == 0) &&
-            (my_strcmp(args[i + 1], ">") == 0 || my_strcmp(args[i + 1], "<") == 0)) {
+    for (int i = 0; args[i] != NULL; i++) {
+        if (((my_strcmp(args[i], ">") == 0 || my_strcmp(args[i], "<") == 0) &&
+        (my_strcmp(args[i + 1], ">") == 0 || my_strcmp(args[i + 1], "<") == 0)) ||
+        ((my_strcmp(args[i], ">>") == 0 || my_strcmp(args[i], "<<") == 0)
+        && (my_strcmp(args[i + 1], ">>") == 0
+        || my_strcmp(args[i + 1], "<<") == 0))) {
             my_printf("Missing name for redirect.\n");
             return (1);
         }
-        if ((my_strcmp(args[i], ">>") == 0 || my_strcmp(args[i], "<<") == 0) &&
-            (my_strcmp(args[i + 1], ">>") == 0 || my_strcmp(args[i + 1], "<<") == 0)) {
-            my_printf("Missing name for redirect.\n");
-            return (1);
-        }
-        if (my_strcmp(args[i], "|" == 0) && my_strcmp(args[i + 1], "|" == 0)) {
+        if (my_strcmp(args[i], "|") == 0 && my_strcmp(args[i + 1], "|") == 0) {
             my_printf("Invalid null command.\n");
             return (1);
         }

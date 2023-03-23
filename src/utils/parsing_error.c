a4fa9472034f3dca18d6dc95a8ff46a36af0d2aa
Jean-Yanis Jeffroy <jean-yanis.jeffroy@epitech.eu>
2023-03-23 11:10:36 +0100

    feat(main) error handling parsing with redirection and pipes

--- src/utils/parsing_error.c
+++ src/utils/parsing_error.c
@@ -19,7 +19,15 @@ int parsing_error(char **args)
             my_printf("Missing name for redirect.\n");
             return (1);
         }
-        i++;
+        if ((my_strcmp(args[i], ">>") == 0 || my_strcmp(args[i], "<<") == 0) &&
+            (my_strcmp(args[i + 1], ">>") == 0 || my_strcmp(args[i + 1], "<<") == 0)) {
+            my_printf("Missing name for redirect.\n");
+            return (1);
+        }
+        if (my_strcmp(args[i], "|" == 0) && my_strcmp(args[i + 1], "|" == 0)) {
+            my_printf("Invalid null command.\n");
+            return (1);
+        }
     }
     return (0);
 }

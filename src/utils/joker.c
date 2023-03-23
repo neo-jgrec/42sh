57f4a00b69e218313f517cd9478d712e97be8aaa
Jean-Yanis Jeffroy <jean-yanis.jeffroy@epitech.eu>
2023-03-23 14:06:43 +0100

    fix(main) norm and opti

--- src/utils/joker.c
+++ src/utils/joker.c
@@ -20,28 +20,6 @@ int is_joker(char c, const char **jokers)
     return 0;
 }
 
-int get_joker_len(const char *str, int index, const char **jokers)
-{
-    int joker_len = 0;
-
-    for (int k = 0; jokers[k] != NULL; k++) {
-        joker_len = my_strlen(jokers[k]);
-        if (my_strncmp_tok((char *)(&str[index]),
-        (char *)(jokers[k]), joker_len) == 0)
-            return joker_len;
-    }
-    return 0;
-}
-
-void make_for_statement(char *str, int *i, int *j, char *new_str)
-{
-    int joker_len = get_joker_len(str, *i, my_jokers);
-
-    for (int k = 0; k < joker_len; k++)
-        new_str[(*j)++] = str[(*i) + k];
-    *i += joker_len - 1;
-}
-
 char *add_space_before_and_after_jokers(char *str)
 {
     char *new_str = malloc(sizeof(char) * my_strlen(str) * 3 + 1);
@@ -51,12 +29,11 @@ char *add_space_before_and_after_jokers(char *str)
         in_quote = (IS_QUOTE(str[i]) ? !in_quote : in_quote);
         if (in_quote || !is_joker(str[i], my_jokers))
             new_str[j++] = str[i];
-        if (!in_quote && is_joker(str[i], my_jokers)) {
-            (i > 0 && !IS_SPACE(str[i - 1])) ? new_str[j++] = ' ' : 0;
-            int joker_len = get_joker_len(str, i, my_jokers);
-            make_for_statement(str, &i, &j, new_str);
-            (str[i + joker_len] != '\0' && !IS_SPACE(str[i + joker_len])) ?
-            new_str[j++] = ' ' : 0;
+        else {
+            (i > 0 && !is_joker(str[i - 1], my_jokers) ? new_str[j++] = ' ' : 0);
+            new_str[j++] = str[i];
+            (str[i + 1] != '\0' && !is_joker(str[i + 1], my_jokers) ?
+            new_str[j++] = ' ' : 0);
         }
     }
     new_str[j] = '\0';

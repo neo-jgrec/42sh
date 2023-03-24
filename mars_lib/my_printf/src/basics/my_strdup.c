f8f5f92d17d156e56aecec819c07ca8338c67ddd
JGrec <jean-yanis.jeffroy@epitech.eu>
2023-03-24 23:13:31 +0100

    fix(main) remove unused function in printf(strdup)

--- mars_lib/my_printf/src/basics/my_strdup.c
+++ mars_lib/my_printf/src/basics/my_strdup.c
@@ -16,14 +16,3 @@ static char *my_strcpy(char *dest, char const *src)
     dest[i] = '\0';
     return dest;
 }
-
-char *my_strdup(char const *src)
-{
-    int size = 0;
-
-    for (int i = 0; src[i] != '\0'; i++)
-        size++;
-    char *duped = malloc(sizeof(char) * size + 1);
-    my_strcpy(duped, src);
-    return duped;
-}

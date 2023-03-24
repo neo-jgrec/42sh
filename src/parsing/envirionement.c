2cce6781f5942effd08331ab5fe578577ccbd52f
JGrec <jean-yanis.jeffroy@epitech.eu>
2023-03-24 23:09:08 +0100

    feat(main) env var fully implemented

--- src/parsing/envirionement.c
+++ src/parsing/envirionement.c
@@ -6,19 +6,32 @@
 */
 
 #include "my.h"
+#include <string.h>
+#include <stdlib.h>
 
-static char *check(char **env, char *name)
+static char *check(char **env, char *name,
+size_t prefix_len, size_t value_len)
 {
-    char *tmp = NULL;
-    if (name[0] == '$') {
-        if (my_getenv(env, name + 1) != NULL) {
-            tmp = my_getenv(env, name + 1);
-            return (tmp);
-        } else {
-            return (NULL);
-        }
+    char *dollar_ptr = my_strchr(name, '$');
+    char *var_name;
+    char *value;
+    char *result;
+
+    if (dollar_ptr == NULL)
+        return name;
+    var_name = dollar_ptr + 1;
+    value = my_getenv(env, var_name);
+    if (value == NULL) {
+        my_printf("%s: Undefined variable.\n", var_name);
+        return NULL;
     }
-    return (name);
+    prefix_len = dollar_ptr - name;
+    value_len = my_strlen(value);
+    result = malloc(prefix_len + value_len + 1);
+    my_strncpy(result, name, prefix_len);
+    my_strncpy(result + prefix_len, value, value_len);
+    result[prefix_len + value_len] = '\0';
+    return result;
 }
 
 char **edit_args_env(char **args, char **env)
@@ -27,11 +40,10 @@ char **edit_args_env(char **args, char **env)
     if (args == NULL)
         return (NULL);
     for (int i = 0; args[i] != NULL; i++) {
-        tmp = check(env, args[i]);
+        tmp = check(env, args[i], 0, 0);
         if (tmp != NULL) {
             args[i] = tmp;
         } else {
-            my_printf("%s: Undefined variable.\n", args[i] + 1);
             return (NULL);
         }
     }

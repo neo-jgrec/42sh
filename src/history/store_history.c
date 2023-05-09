9c5fa54101ae40dfdbbf4c6e4ce1dc0f2cbf2131
LARGEOT MAXENCE <maxence.largeot@epitech.eu>
2023-05-09 17:25:42 +0200

    feat(history): Add the function that store history content

---
+++ src/history/store_history.c
@@ -0,0 +1,58 @@
+/*
+** EPITECH PROJECT, 2023
+** store_history
+** File description:
+** store_history
+*/
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include "my.h"
+
+static void write_into_file(FILE *stream, history_list_t *history)
+{
+    history_t *node = history->head;
+
+    for (; node; node = node->next){
+        printf("%i %s\n", node->pos, node->time);
+        fprintf(stream, "%i %s ", node->pos, node->time);
+        for (size_t i = 0; node->command[i]; i++){
+            fprintf(stream, "%s ", node->command[i]);
+        }
+        fprintf(stream, "\n");
+    }
+}
+
+static void modify_chmod(void)
+{
+    int fd = open("history.txt", O_RDONLY);
+
+    if (fd == -1) {
+        fd = open("history.txt", O_CREAT);
+        close(fd);
+        chmod("history.txt", 0755);
+    } else {
+        close(fd);
+        chmod("history.txt", 0755);
+    }
+    printf("%i\n", fd);
+}
+
+void store_history(history_list_t *history)
+{
+    FILE *fd = NULL;
+
+    if (!history)
+        return;
+    if (history->size == 0) {
+        free(history);
+        return;
+    }
+    modify_chmod();
+    fd = fopen("history.txt", "w+");
+    write_into_file(fd, history);
+    fclose(fd);
+    chmod("history.txt", S_IRUSR);
+    destroy_history(history);
+}

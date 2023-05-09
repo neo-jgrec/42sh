e6ad08ecbdfc334a9591ebe2b47cfc203df70a97
LARGEOT MAXENCE <maxence.largeot@epitech.eu>
2023-05-09 16:16:22 +0200

    refactor(history): manage history get thanks to a file

--- src/history/lst_management/init_history.c
+++ src/history/lst_management/init_history.c
@@ -10,14 +10,51 @@
 #include <sys/stat.h>
 #include "my.h"
 
+static void add_node_to_history(history_list_t *history, char *pos, char *time,
+char **command)
+{
+    history_t *node = malloc(sizeof(history_t));
+
+    if (!node || !history)
+        return;
+    node->pos = atoi(pos);
+    node->time = my_strdup(time);
+    node->command = my_dup_array((const char **)command);
+    node->next = NULL;
+    node->prev = history->tail;
+    if (history->size == 0) {
+        history->head = node;
+        history->tail = node;
+    } else {
+        history->tail->next = node;
+        history->tail = node;
+    }
+    history->size++;
+}
+
+static void add_file_content_to_list(char **commands, history_list_t *history)
+{
+    char **temp = NULL;
+
+    if (!commands)
+        return;
+    for (size_t i = 0; commands[i]; i++) {
+        temp = my_str_tok(commands[i], " ");
+        if (!temp[0] || !temp[1] || !temp[2])
+            continue;
+        add_node_to_history(history, temp[0], temp[1], &temp[2]);
+        my_destroy_str_array(temp);
+    }
+}
+
 static void get_old_history(UNUSED history_list_t *history)
 {
-    int fd = open("save_file/history.txt", O_RDONLY);
+    int fd = open("history.txt", O_RDONLY);
     struct stat stats;
     char *buffer = NULL;
     char **commands = NULL;
 
-    if (fd == -1 || stat("save_file/history.txt", &stats) == -1)
+    if (fd == -1 || stat("history.txt", &stats) == -1)
         return;
     buffer = malloc(sizeof(char) * (stats.st_size + 1));
     if (!buffer)
@@ -29,6 +66,7 @@ static void get_old_history(UNUSED history_list_t *history)
         return;
     }
     commands = my_str_tok(buffer, "\n");
+    add_file_content_to_list(commands, history);
 }
 
 history_list_t *init_history_list(void)

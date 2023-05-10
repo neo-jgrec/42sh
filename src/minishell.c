d3191d08b0a29e696e84fe279a73b635194a5cee
JGrec <jean-yanis.jeffroy@epitech.eu>
2023-05-10 22:57:16 +0200

    fix(main) fix of the stw

--- src/minishell.c
+++ src/minishell.c
@@ -14,7 +14,7 @@ char *clean_str_minishell(char *str, const char *to_clean);
 int execute_commands(char **args, term_t *term);
 int parsing_error(char **args);
 char *read_stdin(term_t *term);
-char **a_mkstw(char *str, char *sep);
+char **stwa(char *str, char *sep);
 
 char *remove_home(char *str, char **env)
 {
@@ -49,7 +49,7 @@ int minishell(char **env)
         if (term.str[0] == '\0')
             continue;
         term.str = clean_str_minishell(term.str, " \t");
-        term.argv = a_mkstw(term.str, " ");
+        term.argv = stwa(term.str, " ");
         manage_history(term.history, term.argv);
         if (term.argv == NULL)
             continue;
--- src/parsing/stw.c
+++ src/parsing/stw.c
@@ -36,8 +36,8 @@ static int count_words(char *str, char *sep)
     for (int i = 0; str[i]; i++) {
         if (str[i] == '"' || str[i] == '\'')
             skip_quote(str, &i, str[i]);
-        if (!in_list(str[i], str[i - 1], sep) &&
-            (in_list(str[i + 1], str[i], sep) || str[i + 1] == '\0'))
+        if ((i == 0 || !in_list(str[i], str[i - 1], sep)) &&
+            (str[i + 1] == '\0' || in_list(str[i + 1], str[i], sep)))
             nb++;
     }
     return nb;
@@ -48,12 +48,15 @@ static int add_word(char *str, char **stw, int word, char *sep)
     int i = 0;
     int len = 0;
 
-    for (;in_list(str[i], str[i - 1], sep) && str[i] != '\0'; i++);
-    for (int j = i; !in_list(str[j], str[j - 1], sep) &&
-        str[j] != '\0'; j++, len++)
+    for (; str[i] != '\0' && in_list(str[i],
+        (i > 0 ? str[i - 1] : '\0'), sep); i++);
+    for (int j = i; str[j] != '\0'
+        && !in_list(str[j], (j > 0 ? str[j - 1] : '\0'), sep); j++, len++)
         if (str[j] == '"' || str[j] == '\'')
             skip_quote_2(str, &j, &len, str[j]);
     stw[word] = malloc(sizeof(char) * (len + 1));
+    if (stw[word] == NULL)
+        return -1;
     for (int j = 0; j < len + 1; j++)
         stw[word][j] = '\0';
     for (int j = 0; j < len; j++)
@@ -65,12 +68,17 @@ char **a_mkstw(char *str, char *sep)
 {
     int nb = count_words(str, sep);
     char **stw = malloc(sizeof(char *) * (nb + 1));
+    if (stw == NULL) return NULL;
     int index = 0;
 
     for (int i = 0; i < nb + 1; i++)
         stw[i] = NULL;
     for (int word = 0; word < nb; word++)
         index += add_word(str + index, stw, word, sep);
+    if (index == -1) {
+        a_freestw(stw);
+        return NULL;
+    }
     for (int i = 0; i < nb; i++)
         clean_quote(stw[i]);
     return stw;

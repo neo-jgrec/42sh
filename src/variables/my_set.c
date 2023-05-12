9ddc7d0179a6a4cadd79cd5bf399e09c01ae88c2
LARGEOT MAXENCE <maxence.largeot@epitech.eu>
2023-05-13 01:22:23 +0200

    refactor(history): Finish set function by handling all possible case

--- src/variables/my_set.c
+++ src/variables/my_set.c
@@ -5,7 +5,22 @@
 ** my_set
 */
 
+#include "my.h"
+
 int my_set(char *name, char *value, char ***var)
 {
+    size_t len = strlen(name);
+    char *new_var = my_strcat_inf(3, name, "=", value);
+
+    for (size_t i = 0; (*var) && (*var)[i]; i++){
+        if (strncmp(name, (*var)[i], len) == 0 && (*var)[i][len] == '=') {
+            free((*var)[i]);
+            (*var)[i] = strdup(new_var);
+            free(new_var);
+            return (0);
+        }
+    }
+    (*var) = my_add_str_to_array((*var), new_var);
+    free(new_var);
     return (0);
 }

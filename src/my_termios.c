732797c503c1c8872574214789996490929a1d5c
Jean-Yanis Jeffroy <jean-yanis.jeffroy@epitech.eu>
2023-05-12 08:50:04 +0200

    feat(main) add of history completion

--- src/my_termios.c
+++ src/my_termios.c
@@ -18,10 +18,12 @@
 
 const term_t *term_ptr;
 const struct handler_args_s *handler_args_ptr;
+const struct termios *orig_termios_ptr;
 
 void disable_raw_mode(struct termios *orig_termios);
 void autocomplete(char *str, size_t *index);
-
+void handle_history(char *str, size_t *index,
+    size_t *history_pos, bool to_head);
 
 void handle_sigint(int sig)
 {
@@ -39,14 +41,16 @@ void handle_sigint(int sig)
     write(STDOUT_FILENO, "^C", 2);
 }
 
-void handle_arrowkeys(char *str, size_t *index)
+void handle_arrowkeys(char *str, size_t *index, size_t *history_pos)
 {
     char seq[3];
 
     if (read(STDIN_FILENO, &seq[0], 1) != 1) return;
     if (read(STDIN_FILENO, &seq[1], 1) != 1) return;
-    if (seq[0] == '[' && seq[1] == 'A') return;
-    if (seq[0] == '[' && seq[1] == 'B') return;
+    if (seq[0] == '[' && seq[1] == 'A')
+        handle_history(str, index, history_pos, true);
+    if (seq[0] == '[' && seq[1] == 'B')
+        handle_history(str, index, history_pos, false);
     if (seq[0] == '[' && seq[1] == 'C') {
         if (*index < strlen(str)) {
             write(STDOUT_FILENO, "\033[C", 3);
@@ -61,8 +65,7 @@ void handle_arrowkeys(char *str, size_t *index)
     }
 }
 
-void handle_action(char *str, size_t *index, char c,
-    struct termios *orig_termios)
+void handle_action(char *str, size_t *index, char c, size_t *history_pos)
 {
     if (c == '\177') {
         if (*index > 0) {
@@ -76,14 +79,14 @@ void handle_action(char *str, size_t *index, char c,
     }
     if (c == '\004') {
         if (strlen(str)) return;
-        disable_raw_mode(orig_termios);
+        disable_raw_mode((struct termios *)orig_termios_ptr);
         printf("exit\n");
         exit((*term_ptr->exit_status == 45) ? 1 : *term_ptr->exit_status);
     }
     if (c == '\t')
         autocomplete(str, index);
     if (c == '\033')
-        handle_arrowkeys(str, index);
+        handle_arrowkeys(str, index, history_pos);
 }
 
 void default_action_case(char *str, size_t *index, char c)
@@ -102,22 +105,21 @@ void process_keypress(char *str, struct termios *orig_termios, term_t *term)
 {
     char c = 0;
     size_t index = 0;
-    handler_args_ptr = &(struct handler_args_s){str, orig_termios, &index, &c};
-    term_ptr = term;
     struct sigaction sa = {.sa_handler = handle_sigint, .sa_flags = 0};
+    size_t *history_pos = &(size_t){0};
+
+    handler_args_ptr = &(struct handler_args_s){str, orig_termios, &index, &c};
+    orig_termios_ptr = orig_termios;
+    term_ptr = term;
     sigemptyset(&sa.sa_mask);
     sigaction(SIGINT, &sa, NULL);
     while (true) {
         read(STDIN_FILENO, &c, 1);
         if (!c || c == '\n') break;
         if (c == '\177' || c == '\004' || c == '\033' || c == '\t')
-            handle_action(str, &index, c, orig_termios);
+            handle_action(str, &index, c, history_pos);
         else
             default_action_case(str, &index, c);
         fflush(stdout);
     }
-    sa = (struct sigaction){.sa_handler = SIG_DFL, .sa_flags = 0\
-    };
-    sigemptyset(&sa.sa_mask);
-    sigaction(SIGINT, &sa, NULL);
 }

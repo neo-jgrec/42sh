b97f8191ef4d471e3a0da03e87f8feb023c320c4
mlargeot <morgan.largeot@epitech.eu>
2023-05-12 15:37:25 +0200

    refactor(parenthese): Add part of null handling

--- src/parenthese/err_handling/handle_null.c
+++ src/parenthese/err_handling/handle_null.c
@@ -5,7 +5,38 @@
 ** handle_null
 */
 
+#include "my.h"
+
+int is_joker(char c, const char **jokers);
+
+const char *token[] = {";", "||", "|", "<<", ">>", ">", "<",
+"&&", "(", ")", NULL};
+
+static bool is_only_token(char *input)
+{
+    for (size_t i = 0; input[i] != '\0'; i++) {
+        if (!is_joker(input[i], token))
+            return false;
+    }
+    return true;
+}
+
+static bool parse_par_command(char *input)
+{
+    if (is_only_token(input))
+        return true;
+    return false;
+}
+
 int handle_null(char **argv)
 {
+    bool ret = false;
+
+    for (size_t i = 0; argv[i] != NULL; i++) {
+        if (argv[i][0] == '(')
+            ret = parse_par_command(argv[i]);
+        if (ret)
+            return print_par_error(4);
+    }
     return 0;
 }

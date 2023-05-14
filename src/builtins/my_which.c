eb0cee22432d0b9d38b39ee7f4d2d6bc848a1f5e
JGrec <jean-yanis.jeffroy@epitech.eu>
2023-05-14 22:27:46 +0200

    fix(main) wich and where

--- src/builtins/my_which.c
+++ src/builtins/my_which.c
@@ -9,21 +9,35 @@
 
 char *get_command_with_path(char *cmd, char **env);
 
+static bool is_builtin(char *cmd)
+{
+    for (int i = 0; commands[i].name != NULL; i++)
+        if (my_strcmp(commands[i].name, cmd) == 0)
+            return (true);
+    return (false);
+}
+
 int my_which(char **args, char **env, int *exit_status,
 UNUSED void *data)
 {
-    char *cmd_with_path = get_command_with_path(args[1], env);
+    char *cmd_with_path = NULL;
 
     if (args[1] == NULL) {
         dprintf(2, "where: Too few arguments.\n");
         return (1);
     }
-    if (cmd_with_path == NULL) {
-        *exit_status = 1;
-        dprintf(2, "%s: Command not found.\n", args[1]);
-        return (1);
+    if (is_builtin(args[1]))
+        printf("%s: shell built-in command.\n", args[1]);
+    else {
+        cmd_with_path = get_command_with_path(args[1], env);
+        if (cmd_with_path == NULL) {
+            *exit_status = 1;
+            dprintf(2, "%s: Command not found.\n", args[1]);
+            return (1);
+        }
+        printf("%s\n", cmd_with_path);
+        free(cmd_with_path);
     }
-    printf("%s\n", cmd_with_path);
     return (0);
 }
 
@@ -40,6 +54,8 @@ UNUSED void *data)
         dprintf(2, "where: Too few arguments.\n");
         return (1);
     }
+    if (is_builtin(args[1]))
+        printf("%s is a shell built-in\n", args[1]);
     for (int i = 0; path_array[i] != NULL; i++) {
         tmp = my_strcat_inf(3, path_array[i], "/", args[1]);
         if (access(tmp, F_OK) == 0)

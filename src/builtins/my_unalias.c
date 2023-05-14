5a2a997476d13a0e83babf74f70d95978df33cf3
Yann Morvan <yann.morvan@epitech.eu>
2023-05-14 15:33:54 +0100

    fix(aliases): Unalias struct

--- src/builtins/my_unalias.c
+++ src/builtins/my_unalias.c
@@ -14,6 +14,7 @@ int my_unalias(char **args, UNUSED char **env, int *exit_status, void *data)
 {
     term_t *term = (term_t *)data;
     struct linked_list_node *node;
+    alias_t *alias_struct;
 
     if (args[1] == NULL) {
         my_printf("unalias: Too few arguments.\n");
@@ -21,7 +22,8 @@ int my_unalias(char **args, UNUSED char **env, int *exit_status, void *data)
         return *exit_status;
     }
     TAILQ_FOREACH(node, &term->alias->head, nodes) {
-        if (strcmp(((char **)node->data)[0], args[1]) == 0) {
+        alias_struct = node->data;
+        if (strcmp(((alias_t *)alias_struct)->command[0], args[1]) == 0) {
             TAILQ_REMOVE(&term->alias->head, node, nodes);
             *exit_status = 0;
             return *exit_status;

019542a2e01aa304caf8a30356722106935965dd
JGrec <jean-yanis.jeffroy@epitech.eu>
2023-05-14 22:17:29 +0200

    feat(parentheses) add of the parentheses feature

--- src/parenthese/my_parenthesis.c
+++ src/parenthese/my_parenthesis.c
@@ -6,23 +6,52 @@
 */
 
 #include "my.h"
+#include <sys/wait.h>
+
+int execute_commands(char **args, term_t *term);
+
+char **extract_commands_in_parentheses(char **args, int *i)
+{
+    int j = 0;
+    char **subshell_args = malloc(sizeof(char *) * 100);
+    int parentheses_count = 0;
+
+    (*i)++;
+    while (args[*i] != NULL) {
+        if (strcmp(args[*i], "(") == 0)
+            parentheses_count++;
+        if (strcmp(args[*i], ")") == 0 && parentheses_count == 0)
+            break;
+        if (strcmp(args[*i], ")") == 0 && parentheses_count > 0)
+            parentheses_count--;
+        subshell_args[j] = strdup(args[*i]);
+        j++;
+        (*i)++;
+    }
+
+    subshell_args[j] = NULL;
+    return subshell_args;
+}
 
 void my_parenthesis(UNUSED exec_t *exec, int *i, term_t *term, char **args)
 {
-    char **par_input = calloc(100, sizeof(char *));
-    size_t nb = 1;
-    size_t index = 0;
+    int pid, status;
+    char **subshell_args;
 
-    for (size_t j = *i + 1; nb != 0; j++) {
-        if (!strcmp(args[j], "("))
-            nb ++;
-        if (!strcmp(args[j], ")"))
-            nb --;
-        if (nb != 0)
-            par_input[index++] = strdup(args[j]);
+    if (args[*i + 1] == NULL || strcmp(args[*i + 1], ")") == 0) {
+        dprintf(2, "Missing command for subshell.\n");
+        remove_element_at_index(args, *i);
+        return;
     }
-    exec_parenthesis(par_input, term->env);
-    *i += index + 1;
+    subshell_args = extract_commands_in_parentheses(args, i);
+    if ((pid = fork()) < 0) {
+        perror_exit("fork");
+    } else if (pid == 0) {
+        execute_commands(subshell_args, term);
+        exit(0);
+    } else
+        waitpid(pid, &status, 0);
+    (*i) += 1;
     exec->cmd_start = *i + 1;
-    return;
+    free(subshell_args);
 }
